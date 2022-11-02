de727d6701db3cd5dcbed11633b49d9970b20001
PRIYANSHU GUPTA <shamugupta8259@gmail.com>
2022-11-02 19:39:10 +0530

    prac

---
+++ App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Cards from './components/card';
+function App() {
+    return (
+      <div>
+        <Cards  title="javascript" src="https://images.pexels.com/photos/792199/pexels-photo-792199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+        
+      </div>
+      );
+
+     
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tw config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
